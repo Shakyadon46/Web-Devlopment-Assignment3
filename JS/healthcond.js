e4f987f7e4d217b359846de88db9b88ce71fe546
Aayush Shakya <aayushshakya2000@gmail.com>
2024-10-21 21:45:36 +0545

    Commit Webiste
    Mental Health

---
+++ JS/healthcond.js
@@ -0,0 +1,35 @@
+/*for navbar*/
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+
+function toggleDescription(id, element) {
+       const description = document.getElementById(id);
+       const icon = element.querySelector('i');
+       
+       if (description.style.display === "none") {
+           description.style.display = "block";
+           icon.classList.add('rotate');
+       } else {
+           description.style.display = "none";
+           icon.classList.remove('rotate');
+       }
+   }
+
+   function showQuizResult() {
+const anxiety = document.querySelector('input[name="anxiety"]:checked').value;
+const focus = document.querySelector('input[name="focus"]:checked').value;
+const depression = document.querySelector('input[name="depression"]:checked').value;
+let result = '';
+
+if (anxiety === 'yes' || focus === 'yes' || depression === 'yes') {
+result = "It seems you may be experiencing some mental health challenges. Consider seeking support.";
+} else {
+result = "You're doing well! Keep maintaining your mental health.";
+}
+
+document.getElementById('quiz-result').innerText = result;
+}
